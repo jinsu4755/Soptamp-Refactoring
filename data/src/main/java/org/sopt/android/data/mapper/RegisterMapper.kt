93e6b1a9e0483625c19fc0bb5a429a35dcd4acb4
jinsu4755 <parkjinsu4755@gmail.com>
2023-02-13 02:16:11 +0900

    feat: 회원가입 모듈간 의존성을 낮추기위한 객체 변환용 mapper 클래스 작성

--- data/src/main/java/org/sopt/android/data/mapper/RegisterMapper.kt
+++ data/src/main/java/org/sopt/android/data/mapper/RegisterMapper.kt
@@ -1,1 +1,8 @@
 package org.sopt.android.data.mapper
+
+import org.sopt.android.data.model.RegisterData
+import org.sopt.android.domain.model.Register
+
+fun RegisterData.toDomain() = Register(
+    this.userId
+)
