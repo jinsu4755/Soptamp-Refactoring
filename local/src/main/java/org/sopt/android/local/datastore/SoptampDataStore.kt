7c4219958b940b62e42be1e82766d61debe5f1c2
jinsu4755 <parkjinsu4755@gmail.com>
2023-02-13 03:04:07 +0900

    style: datastore 위치 변경

--- local/src/main/java/org/sopt/android/local/datastore/SoptampDataStore.kt
+++ local/src/main/java/org/sopt/android/local/datastore/SoptampDataStore.kt
@@ -1,4 +1,4 @@
-package org.sopt.android.local
+package org.sopt.android.local.datastore
 
 import android.content.Context
 import androidx.datastore.preferences.core.edit
@@ -12,11 +12,11 @@ import javax.inject.Inject
 
 private val Context.dataStore by preferencesDataStore(name = "pref")
 
-class SoptampDataStore @Inject constructor(
+internal class SoptampDataStore @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
     val userId: Flow<Long> = context.dataStore.data.map { preferences -> preferences[USER_ID] ?: -1 }
-    val profileMessage: Flow<String> = context.dataStore.data.map { preferences -> preferences[PROFILE_MESSAGE] ?: "" }
+    val profileMessage: Flow<String?> = context.dataStore.data.map { preferences -> preferences[PROFILE_MESSAGE] ?: "" }
 
     suspend fun setUserId(userId: Long) {
         context.dataStore.edit { pref ->
@@ -24,14 +24,15 @@ class SoptampDataStore @Inject constructor(
         }
     }
 
-    suspend fun setProfileMessage(message: String) {
+    suspend fun setProfileMessage(message: String?) {
         context.dataStore.edit { preferences ->
-            preferences[PROFILE_MESSAGE] = message
+            preferences[PROFILE_MESSAGE] = message ?: NULL_PROFILE_MESSAGE
         }
     }
 
     companion object {
         private val USER_ID = longPreferencesKey("user")
         private val PROFILE_MESSAGE = stringPreferencesKey("profile_message")
+        const val NULL_PROFILE_MESSAGE = "설정된 한 마디가 없습니다"
     }
 }
